def76bf3ab2166d4b33a7065f21000552bcfffe7
Martin Wilson <martinwilson450@gmail.com>
2020-02-11 17:46:11 -0800

    added login page and updated gradle build

--- app/src/main/java/com/example/postertocalendar/LoginPage.java
+++ app/src/main/java/com/example/postertocalendar/LoginPage.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class imageView extends AppCompatActivity {
+public class LoginPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image_view);
+        setContentView(R.layout.activity_login_page);
     }
 }
